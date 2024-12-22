00d584469bddd2333f4ddc8ab770c217fff0f9e7
olrlozl <thakd221@naver.com>
2024-12-22 22:57:45 +0900

    feat: Modal 퍼블리싱

--- frontend/src/components/common/Modal/Modal.tsx
+++ frontend/src/components/common/Modal/Modal.tsx
@@ -1,1 +1,45 @@
-export {};
+import React from 'react';
+import ReactDOM from 'react-dom';
+import OverLay from './OverLay';
+import '../../../styles/components/common/Modal/modal.scss';
+
+interface ModalProps {
+  isOpen: boolean;
+  imageSrc?: string;
+  message: string;
+  buttons: { label: string; onClick: () => void }[];
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  imageSrc,
+  message,
+  buttons,
+}) => {
+  if (!isOpen) return null;
+
+  return ReactDOM.createPortal(
+    <OverLay>
+      <div className="modal-container" onClick={(e) => e.stopPropagation()}>
+        {imageSrc && (
+          <img className="modal-image" src={imageSrc} alt="Modal Visual" />
+        )}
+        <div className="modal-message">{message}</div>
+        <div className="modal-buttons">
+          {buttons.map((button, index) => (
+            <button
+              key={index}
+              className="modal-button"
+              onClick={button.onClick}
+            >
+              {button.label}
+            </button>
+          ))}
+        </div>
+      </div>
+    </OverLay>,
+    document.getElementById('modal-root') as HTMLElement
+  );
+};
+
+export default Modal;
--- frontend/src/pages/PlayPage.tsx
+++ frontend/src/pages/PlayPage.tsx
@@ -1,1 +1,32 @@
-export {};
+import React, { useState } from 'react';
+import Modal from '../components/common/Modal/Modal';
+import eggwin from '../../src/assets/images/egg-win.png';
+
+const PlayPage = () => {
+  //// [Modal 사용예시]
+  const [isModalOpen, setModalOpen] = useState(true);
+  // const openModal = () => {
+  //   setModalOpen(true);
+  // };
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+  const rematch = () => {
+    closeModal();
+  };
+  return (
+    <div>
+      <Modal
+        isOpen={isModalOpen}
+        imageSrc={eggwin}
+        message="아리 승!"
+        buttons={[
+          { label: '재대결', onClick: rematch },
+          { label: '나가기', onClick: closeModal },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default PlayPage;
