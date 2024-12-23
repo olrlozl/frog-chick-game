54606b626dbd6ce739f67ff9725fa0b9600eb593
olrlozl <thakd221@naver.com>
2024-12-23 20:08:51 +0900

    feat: React Router 설정 및 페이지 라우트 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './App.css';
+import LandingPage from 'pages/LandingPage';
+import MainPage from 'pages/MainPage';
+import PlayPage from 'pages/PlayPage';
+
+const router = createBrowserRouter([
+  { path: '/', element: <LandingPage /> },
+  { path: '/main', element: <MainPage /> },
+  { path: '/play', element: <PlayPage /> },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
