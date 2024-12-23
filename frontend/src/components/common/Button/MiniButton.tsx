e39f8a718db1df601c2a55c3208ef3564ddf8220
JangHyunsu <wkd940709@naver.com>
2024-12-23 23:57:51 +0900

    feat: mini-button 퍼블리싱

--- frontend/src/components/common/Button/MiniButton.tsx
+++ frontend/src/components/common/Button/MiniButton.tsx
@@ -1,1 +1,45 @@
-export {};
+import 'styles/components/common/Button/mini-button.scss';
+
+interface MiniButtonProps {
+  option: 'search' | 'add' | 'game' | 'accept' | 'reject';
+}
+
+const MiniButton = ({option}: MiniButtonProps) => {
+  
+  const handleClickSearchNickname = () => {
+    
+  }
+
+  const handleClickAddFriend = () => {
+    
+  }
+
+  const handleClickRequestGame = () => {
+    
+  }
+
+  const handleClickAcceptFriend = () => {
+    
+  }
+
+  const handleClickRejectFriend = () => {
+    
+  }
+
+  const options = {
+    search: { text: '검색', onClick: handleClickSearchNickname},
+    add: { text: '친구추가', onClick: handleClickAddFriend},
+    game: { text: '대결신청', onClick: handleClickRequestGame},
+    accept: { text: '수락', onClick: handleClickAcceptFriend},
+    reject: { text: '거절', onClick: handleClickRejectFriend},
+  }
+  const { onClick, text } = options[option];
+
+  return (
+    <div className={`mini-button ${option}`} onClick={onClick}>
+      {text}
+    </div>
+  )
+}
+
+export default MiniButton
