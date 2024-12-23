6d94807235ed33c5cc1560dcda59fd00d5a3e07c
olrlozl <thakd221@naver.com>
2024-12-23 17:48:22 +0900

    feat: Modal에 messageFontSize 속성 추가 및 동적 스타일 적용

--- frontend/src/components/common/Modal/Modal.tsx
+++ frontend/src/components/common/Modal/Modal.tsx
@@ -7,16 +7,23 @@ interface ModalProps {
   isOpen: boolean;
   imageSrc?: string;
   message: string;
+  messageFontSize?: 'font-xl' | 'font-md';
   btns: { label: string; onClick: () => void; type: 'primary' | 'secondary' }[];
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, imageSrc, message, btns }) => {
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  imageSrc,
+  message,
+  messageFontSize,
+  btns,
+}) => {
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
     <div className="modal" onClick={(e) => e.stopPropagation()}>
       {imageSrc && <img className="image" src={imageSrc} alt="Modal Visual" />}
-      <div className="message">{message}</div>
+      <div className={`message ${messageFontSize}`}>{message}</div>
       <div className="buttons">
         {btns.map((btn, index) => (
           <ModalButton
--- frontend/src/pages/PlayPage.tsx
+++ frontend/src/pages/PlayPage.tsx
@@ -23,6 +23,7 @@ const PlayPage = () => {
           isOpen={isModalOpen}
           imageSrc={eggwin}
           message="아리 승!"
+          messageFontSize="font-xl"
           btns={[
             { label: '재대결', onClick: rematch, type: 'primary' },
             { label: '나가기', onClick: closeModal, type: 'secondary' },
