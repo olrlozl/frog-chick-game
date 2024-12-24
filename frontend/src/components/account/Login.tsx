096884e07fbd6c58238cc2194f1b11d6c82684dd
JangHyunsu <wkd940709@naver.com>
2024-12-24 17:48:22 +0900

    feat: kakao 로그인 버튼 퍼블리싱싱

--- frontend/src/components/account/Login.tsx
+++ frontend/src/components/account/Login.tsx
@@ -1,1 +1,13 @@
-export {};
+import kakao from 'assets/images/kakao.png';
+import 'styles/components/account/login.scss';
+
+const Login = () => {
+  return (
+    <div className='login'>
+      <img src={kakao} alt="" />
+      <span>카카오로 3초 만에 시작하기</span>
+    </div>
+  )
+}
+
+export default Login
