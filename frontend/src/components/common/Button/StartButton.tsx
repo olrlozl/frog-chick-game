e77d89c3fb6715147dc369ce36587b3412380bd0
JangHyunsu <wkd940709@naver.com>
2024-12-24 11:00:26 +0900

    feat: start-button 퍼블리싱싱

--- frontend/src/components/common/Button/StartButton.tsx
+++ frontend/src/components/common/Button/StartButton.tsx
@@ -1,1 +1,32 @@
-export {};
+import 'styles/components/common/Button/start-button.scss';
+import frog from 'assets/images/frog.png';
+import chick from 'assets/images/chick.png';
+
+interface StartButtonProps {
+  option: 'stranger' | 'friend';
+  onClick: () => void;
+  isSelected: boolean;
+}
+
+const StartButton = ({ option, onClick, isSelected }: StartButtonProps) => {
+  const options = {
+    stranger: { image: frog, texts: ['낯선이와', '게임하기'] },
+    friend: { image: chick, texts: ['친구와', '게임하기'] },
+  };
+
+  const { image, texts } = options[option];
+
+  return (
+    <div
+      className={`start-button ${option} ${isSelected && 'selected'}`}
+      onClick={onClick}
+    >
+      <img className={option} src={image} alt="" />
+      {texts.map((text) => (
+        <div>{text}</div>
+      ))}
+    </div>
+  );
+};
+
+export default StartButton;
