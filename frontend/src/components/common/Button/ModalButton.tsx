7289b820fd2c446245dbf540f2c914dac09f29d2
JangHyunsu <wkd940709@naver.com>
2024-12-25 00:57:55 +0900

    chore: ModalButton 타입 추가

--- frontend/src/components/common/Button/ModalButton.tsx
+++ frontend/src/components/common/Button/ModalButton.tsx
@@ -3,7 +3,7 @@ import 'styles/components/common/Button/modal-button.scss';
 interface ModalButtonProps {
   label: string;
   onClick: () => void;
-  type: 'primary' | 'secondary';
+  type: 'primary' | 'secondary' | 'cancel';
 }
 
 const ModalButton = ({ label, onClick, type }: ModalButtonProps) => {
