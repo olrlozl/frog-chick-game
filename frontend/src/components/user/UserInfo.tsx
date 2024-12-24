7c57e14ee8b2e2872bb2240b7424eb0763699914
JangHyunsu <wkd940709@naver.com>
2024-12-25 00:59:05 +0900

    feat: UserInfo 퍼블리싱

--- frontend/src/components/user/UserInfo.tsx
+++ frontend/src/components/user/UserInfo.tsx
@@ -1,1 +1,22 @@
-export {};
+import 'styles/components/user/user-info.scss';
+
+interface UserInfoProps {
+  option: 'search' | 'list';
+  nickname: string;
+  win: number;
+  lose: number;
+}
+
+const UserInfo = ({option, nickname, win, lose}: UserInfoProps) => {
+  return (
+    <div className='user-info'>
+      <span className={`nickname ${option}`}>{nickname}</span>
+      <div className={`win-lose-box ${option}`}>
+        <span>{win}승</span>
+        <span>{lose}패</span>
+      </div>
+    </div>
+  )
+}
+
+export default UserInfo
