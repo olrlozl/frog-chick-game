92b1f84e5ea70dd9befa85779c04358775585676
JangHyunsu <wkd940709@naver.com>
2024-12-26 20:05:32 +0900

    chore: 경로 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import LandingPage from 'pages/LandingPage';
 import MainPage from 'pages/MainPage';
 import PlayPage from 'pages/PlayPage';
+import RankingPage from 'pages/RankingPage';
 
 const router = createBrowserRouter([
   { path: '/', element: <LandingPage /> },
-  { path: '/main', element: <MainPage /> },
   { path: '/play', element: <PlayPage /> },
+  {
+    path: '/main',
+    children: [
+      {
+        path: '',
+        element: <MainPage />,
+      },
+      {
+        path: 'ranking',
+        element: <RankingPage />,
+      },
+    ],
+  },
 ]);
 
 function App() {
