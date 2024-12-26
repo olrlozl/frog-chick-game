4feb6dfd7a8b75904d2c20a6dca27421fb9be86e
JangHyunsu <wkd940709@naver.com>
2024-12-26 15:56:50 +0900

    chore: 제한된 문자열에서 string으로 수정

--- frontend/src/components/user/BalloonTitle.tsx
+++ frontend/src/components/user/BalloonTitle.tsx
@@ -1,7 +1,7 @@
 import 'styles/components/user/balloon-title.scss';
 
 interface BalloonTitleProps {
-  title: '친구 검색' | '친구 목록' | '받은 요청' | '낯선이를 탐색중입니다...';
+  title: string;
 }
 
 const BalloonTitle = ({ title }: BalloonTitleProps) => {
--- frontend/src/components/user/NicknameInput.tsx
+++ frontend/src/components/user/NicknameInput.tsx
@@ -1,7 +1,7 @@
 import 'styles/components/user/nickname-input.scss';
 
 interface NicknameInputProps {
-  text: '한글, 영어 2~6자' | '닉네임을 입력해주세요.'
+  text: string;
 }
 
 const NicknameInput = ({ text }: NicknameInputProps) => {
