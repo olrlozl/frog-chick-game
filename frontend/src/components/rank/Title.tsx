bc6de4b5be9814156feb61959c08520edf587e80
JangHyunsu <wkd940709@naver.com>
2024-12-27 01:40:03 +0900

    chore: Title 컴포넌트 경로 이동

---
+++ frontend/src/components/rank/Title.tsx
@@ -0,0 +1,11 @@
+import 'styles/components/rank/title.scss';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <label>Top 20</label>
+    </div>
+  );
+};
+
+export default Title;
