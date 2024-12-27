ff6b0356306124011019ff7e4c10c969e2051f85
JangHyunsu <wkd940709@naver.com>
2024-12-27 21:34:18 +0900

    feat: GuidePage 퍼블리싱

---
+++ frontend/src/pages/GuidePage.tsx
@@ -0,0 +1,27 @@
+import Logout from 'components/account/Logout';
+import GuideBalloon from 'components/guide/GuideBalloon';
+import 'styles/pages/guide-page.scss';
+import farmer from 'assets/images/farmer.png';
+import pond from 'assets/images/pond.png';
+import { useState } from 'react';
+import { GuideOptionType } from 'types/guide';
+
+const GuidePage = () => {
+  const [selectedOption, setSelectedOption] = useState<GuideOptionType>('rule');
+
+  let guideImage = selectedOption === 'rule' ? pond : farmer;
+
+  const toggleSelectedOption = () => {
+    setSelectedOption((prev) => (prev === 'rule' ? 'control' : 'rule'));
+  };
+
+  return (
+    <div className="guide-page">
+      <GuideBalloon option={selectedOption} onClick={toggleSelectedOption} />
+      <img src={guideImage} alt="" />
+      <Logout />
+    </div>
+  );
+};
+
+export default GuidePage;
