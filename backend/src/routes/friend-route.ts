67d686a763e60bacebdb2fd9c8dcfbef37eb4c89
olrlozl <thakd221@naver.com>
2025-06-11 02:26:36 +0900

    feat: 친구수락 api 개발

--- backend/src/routes/friend-route.ts
+++ backend/src/routes/friend-route.ts
@@ -5,6 +5,7 @@ import {
   cancelApplyFriend,
   getFriendList,
   getReceivedFriendList,
+  acceptFriend,
 } from '../controllers/friend-controller';
 
 const friendRouter = Router();
@@ -14,5 +15,6 @@ friendRouter.post('/apply', applyFriend);
 friendRouter.delete('/apply/:nickname', cancelApplyFriend);
 friendRouter.get('/', getFriendList);
 friendRouter.get('/receipts', getReceivedFriendList);
+friendRouter.post('/accept/:nickname', acceptFriend);
 
 export default friendRouter;
