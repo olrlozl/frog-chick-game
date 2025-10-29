b48eb9a98871d7f759f4836d61e1a37f6eac75f6
olrlozl <thakd221@naver.com>
2025-10-29 15:43:46 +0900

    fix: 오디오 파일 경로 수정으로 NotSupportedError 해결

--- frontend/src/constants/play.ts
+++ frontend/src/constants/play.ts
@@ -3,10 +3,10 @@ export const TURN_TIME_LIMIT = 10;
 export const START_COUNT = 3;
 
 export const SOUND_PATHS = {
-  placeSuccess: 'assets/sounds/place-success.mp3',
-  placeFail: 'assets/sounds/place-fail.mp3',
-  timeOver: 'assets/sounds/big-bubble.mp3',
-  touchLock: 'assets/sounds/notification-error.mp3',
-  countDown: 'assets/sounds/countdown.mp3',
-  bingo: 'assets/sounds/yipee.mp3',
+  placeSuccess: '/assets/sounds/place-success.mp3',
+  placeFail: '/assets/sounds/place-fail.mp3',
+  timeOver: '/assets/sounds/big-bubble.mp3',
+  touchLock: '/assets/sounds/notification-error.mp3',
+  countDown: '/assets/sounds/countdown.mp3',
+  bingo: '/assets/sounds/yipee.mp3',
 };
