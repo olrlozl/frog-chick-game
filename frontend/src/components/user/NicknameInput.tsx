f3bd0f2eaced0150071cc0577d00a44e721a7c10
olrlozl <thakd221@naver.com>
2025-12-14 01:29:39 +0900

    feat: 닉네임 입력값 초기화 버튼 구현

--- frontend/src/components/user/NicknameInput.tsx
+++ frontend/src/components/user/NicknameInput.tsx
@@ -13,32 +13,49 @@ interface NicknameInputProps {
   onEnter: () => void;
 }
 
-const NicknameInput = ({ text, nickname, setNickname, setErrorMessage, onEnter }: NicknameInputProps) => {
+const NicknameInput = ({
+  text,
+  nickname,
+  setNickname,
+  setErrorMessage,
+  onEnter,
+}: NicknameInputProps) => {
   const handleChangeNickname = (e: ChangeEvent) => {
     setErrorMessage('');
-    if (e.target.value.length > NICKNAME_MAX_LENGTH) {
-      setNickname(e.target.value.slice(0, NICKNAME_MAX_LENGTH));
-    } else {
-      setNickname(e.target.value)
-    }
-  }
+
+    setNickname(e.target.value);
+  };
 
   const executeOnEnter: KeyboardEventHandler<HTMLInputElement> = (e) => {
     if (e.key === 'Enter') onEnter();
-  }
+  };
 
   return (
-    <input
-      className='nickname-input'
-      type="text"
-      placeholder={text}
-      value={nickname}
-      onChange={(e) => handleChangeNickname(e)}
-      onKeyDown={executeOnEnter}
-      pattern="^[가-힣a-zA-Z]{2,6}$"
-      minLength={NICKNAME_MIN_LENGTH}
-      maxLength={NICKNAME_MAX_LENGTH}
-    />
+    <div className="nickname-input">
+      <input
+        type="text"
+        placeholder={text}
+        value={nickname}
+        onChange={(e) => handleChangeNickname(e)}
+        onKeyDown={executeOnEnter}
+        pattern="^[가-힣a-zA-Z]{2,6}$"
+        minLength={NICKNAME_MIN_LENGTH}
+        maxLength={NICKNAME_MAX_LENGTH}
+      />
+
+      <div className="clear-btn">
+        <button
+          type="button"
+          className={nickname ? '' : 'hidden'}
+          onClick={() => {
+            setNickname('');
+            setErrorMessage('');
+          }}
+        >
+          X
+        </button>
+      </div>
+    </div>
   );
 };
 
